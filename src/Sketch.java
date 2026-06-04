84a2f639b7eb06e04f3433093b18bc46a5e3c782
marlak-1 <marla.kamil27@ycdsbk12.ca>
2026-06-04 14:51:55 -0400

    Implement button to start the race

--- src/Sketch.java
+++ src/Sketch.java
@@ -29,17 +29,11 @@ public class Sketch extends PApplet {
     @Override
     public void draw() {
         background(96, 193, 237);
-
-        fill(49, 113, 28);  // Green
-        rect(width, height, 0, 350);
-        fill(255);
-        rect(200, 200, 400, 400);  // Bird placeholder
-        rect(200, 450, 400, 550);  // Feed button
-        
-        fill(0);
-        textSize(25);
-        text("Click to FEED!", width / 2, 510);
-        text("Running speed: " + playerSpeed, width / 2, 50);
+        if (!isRacing) {
+            drawHome();
+        } else {
+            drawTrack();
+        }
     }
 
     public void mouseClicked() {
@@ -47,7 +41,35 @@ public class Sketch extends PApplet {
             if (mouseX > 200 && mouseX < 400 && mouseY > 450 && mouseY < 550) {
                 playerSpeed++;
                 System.out.println("Speed: " + playerSpeed);
+            } else if (mouseX > 450 && mouseY > 450) {
+                isRacing = true;
             }
         }
+    }
+
+    public void drawButtons() {
+        fill(255);
+        rect(200, 450, 400, 550);  // Feed button
+        rect(450, 450, width, height);  // Race button
+
+        fill(0);
+        textSize(27);
+        text("Click to FEED!", width / 2, 510);
+        text("RACE!", 525, 535);
+    }
+
+    public void drawTrack() {
+        fill(49, 113, 28);  // Green
+        rect(width, height, 0, 50);  // Grass
+        fill(118, 151, 27);
+        rect(0, 125, width, 475);  // Track 
+    }
+
+    public void drawHome() {
+        fill(49, 113, 28);  // Green
+        rect(width, height, 0, 350);  // Grass
+        drawButtons();
+        rect(200, 200, 400, 400);  // Bird placeholder
+        text("Running speed: " + playerSpeed, width / 2, 50);
     }
 }
