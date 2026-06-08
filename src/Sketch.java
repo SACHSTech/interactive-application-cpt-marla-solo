1c299d1a40c1391ba396e5cdb596fc08116304b6
marlak-1 <marla.kamil27@ycdsbk12.ca>
2026-06-08 07:59:48 -0400

    Refactored code to work with any window size and removed running animations for optimization

--- src/Sketch.java
+++ src/Sketch.java
@@ -9,15 +9,19 @@ import processing.core.PImage;
 public class Sketch extends PApplet {
     boolean isRacing = false;
     float playerSpeed = 1;
-    int gooseFrame = 0;
+    float canadaSpeed = random(50, 70);
+    int gooseFrame = 1;
     float finishLine;
 
     // Goose image/animations
     PImage embdenIdle;
     PImage[] embdenRun = new PImage[4];
+    PImage[] canadaRun = new PImage[4];
 
     // Misc. images
-    // PImage tree;
+    PImage tree;
+    PImage bush;
+    PImage dandelions;
 
     public static void main(String[] args) {
         PApplet.main("Sketch");
@@ -25,7 +29,7 @@ public class Sketch extends PApplet {
 
     @Override
     public void settings() {
-        size(600, 600); 
+        size(400, 400); 
     }
 
     @Override
@@ -37,11 +41,14 @@ public class Sketch extends PApplet {
 
         // Load images from the images/ folder
         embdenIdle = loadImage("images/embden-idle.png");
-        // tree = loadImage("images/tree.png");
+        tree = loadImage("images/tree.png");
+        bush = loadImage("images/bush.png");
+        dandelions = loadImage("images/dandelions.png");
 
         // Load goose running animations
         for (int i = 0; i < 4; i++) {
             embdenRun[i] = loadImage("images/embden-run" + i + ".png");
+            canadaRun[i] = loadImage("images/canada-run" + i + ".png");
         }
     }
 
@@ -56,7 +63,8 @@ public class Sketch extends PApplet {
         else {
             drawTrack();
             endTrack();
-            animateGoose(embdenRun, 300, 300, 70);
+            animateGoose(embdenRun, width / 2, height / 2, 70);
+            animateGoose(canadaRun, width / 2, 120, 70);
         }
 
         drawText();
@@ -64,13 +72,13 @@ public class Sketch extends PApplet {
 
     public void mouseClicked() {
         if (!isRacing) {
-            if (mouseX > 200 && mouseX < 400 && mouseY > 450 && mouseY < 550) {
+            if (mouseX > width / 3 && mouseX < 2 * (width / 3) && mouseY > 3 * (height / 4) && mouseY < height) {
                 playerSpeed++;
                 System.out.println("Speed: " + playerSpeed);
             } 
             
             // "RACE!" button
-            else if (mouseX > 450 && mouseY > 450) {
+            else if (mouseX > 3 * (width / 4) && mouseY > 3 * (height / 4)) {
                 isRacing = true;
                 finishLine = 1000;
             }
@@ -78,53 +86,53 @@ public class Sketch extends PApplet {
     }
 
     public void drawText() {
-        textSize(27);
+        textSize(20);
         fill(0);
 
         // HOME
         if (!isRacing) {
-            text("Click to FEED!", width / 2, 510);
-            text("RACE!", 525, 535);
-            text("Running speed: " + playerSpeed, width / 2, 50);
+            text("Click to FEED!", width / 2, (height / 30) * 27);
+            text("RACE!", 27 * (width / 30), 27 * (height / 30));
+            text("Running speed: " + playerSpeed, width / 2, height / 8);
         }
 
         // RACE
         if (isRacing) {
-            text("Press X to leave the race.", width / 2, 35);
+            text("Press X to leave the race.", width / 2, height / 14);
         }
     }
 
     public void drawTrack() {
         fill(49, 113, 28);  // Green
-        rect(width, height, 0, 50);  // Grass
+        rect(width, height, 0, height / 10);  // Grass
         fill(118, 151, 27);
-        rect(0, 125, width, 475);  // Track 
+        rect(0, height / (float)4.8, width, height / (float)1.26);  // Track 
         fill(17, 68, 21); 
-        rect(width, height, 0, 550);  // Side
+        rect(width, height, 0, 9 * (height / 10));  // Side
     }
 
     public void drawHome() {
         // Background
         fill(49, 113, 28);  // Green
-        rect(width, height, 0, 350);  // Grass
+        rect(width, height, 0, height / 2);  // Grass
 
         // Buttons
         fill(151, 118, 139);  // Muted purple
-        rect(200, 450, 400, 550);  // Feed button
-        rect(450, 450, width, height);  // Race button
+        rect(width / 3, height, 2 * (width / 3), 3 * (height / 4));  // Feed button
+        rect(3 * (width / 4), 3 * (height / 4), width, height);  // Race button
 
-        image(embdenIdle, 300, 300, 200, 200);
+        image(embdenIdle, width / 2, height / 2, 150, 150);
     }
 
     public void animateGoose(PImage[] gooseRun, float gooseX, float gooseY, float gooseSize) {
         // Switch the running frame once every 15 frames
-        if (frameCount % 15 == 0) {
+        if (frameCount % 10 == 0) {
             // Restart animation at the last frame
-            if (gooseFrame == 3) {
-                gooseFrame = -1;
-            }
+            // if (gooseFrame == 3) {
+            //     gooseFrame = -1;
+            // }
 
-            gooseFrame++;
+            // gooseFrame++;
         }
 
         image(gooseRun[gooseFrame], gooseX, gooseY, gooseSize, gooseSize);
@@ -135,7 +143,7 @@ public class Sketch extends PApplet {
         System.out.println("Finish line x: " + finishLine);
 
         fill(180, 62, 62);  // Red
-        rect(finishLine, 50, finishLine - 20, 550);  // Finish line
+        rect(finishLine, height / 10, finishLine - 20, 9 * (height / 10));  // Finish line
 
         if (finishLine < width / 2) {
             isRacing = false;
